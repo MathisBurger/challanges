101729e67798a2ec2303a293afd71ecd1b266321
Mathis Burger <handymathis@gmail.com>
2024-05-05 23:55:38 +0200

    [FIX] Added newer version of spigot API

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.17-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
     testImplementation(kotlin("test"))
 }
 
